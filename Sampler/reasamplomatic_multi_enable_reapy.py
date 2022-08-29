2cf98bc51d428ad0d130b09ebcc7d7559fe89762
Maxim <maximvdberg@gmail.com>
2022-08-29 21:03:33 +0200

    add enable reapy action

--- Sampler/reasamplomatic_multi_enable_reapy.py
+++ Sampler/reasamplomatic_multi_enable_reapy.py
@@ -1,4 +1,7 @@
-# @provides [main] .
+# @description Enable reapy
+# @version 1.0
+# @author maxim
+# @about Run this script to enable reapy from within REAPER.
 
 import reapy
 reapy.config.enable_dist_api()
